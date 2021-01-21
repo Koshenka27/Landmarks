1bfcb06587a3f3856214b6b91eeb667e2044ae60
Logan Koshenka <koshenka27@gmail.com>
2021-01-20 22:06:52 -0500

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Logan  Koshenka on 1/20/21.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
