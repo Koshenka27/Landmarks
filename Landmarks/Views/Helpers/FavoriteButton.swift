4f8434ca5da9e299a25c3d7f45104bd52c7196fa
Logan Koshenka <koshenka27@gmail.com>
2021-01-27 15:15:53 -0500

    adding everything up until apple watch / mac targets

---
+++ Landmarks/Views/Helpers/FavoriteButton.swift
@@ -0,0 +1,25 @@
+//
+//  FavoriteButton.swift
+//  Landmarks
+//
+//  Created by Logan  Koshenka on 1/20/21.
+//
+
+import SwiftUI
+
+struct FavoriteButton: View {
+    @Binding var isSet: Bool
+    
+    var body: some View {
+        Button(action: { isSet.toggle() }, label: {
+            Image(systemName: isSet ? "star.fill" : "star")
+                .foregroundColor(isSet ? Color.yellow : Color.gray)
+        })
+    }
+}
+
+struct FavoriteButton_Previews: PreviewProvider {
+    static var previews: some View {
+        FavoriteButton(isSet: .constant(true))
+    }
+}
